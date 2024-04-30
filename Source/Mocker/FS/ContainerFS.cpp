3bb211a3a93296d770180007a1f9a5ef9692279f
Yogesh Aggarwal <yogeshdevaggarwal@gmail.com>
2024-04-30 21:07:09 +0530

    Added files for `ContainerFS` with basic functions related to class members

---
+++ Source/Mocker/FS/ContainerFS.cpp
@@ -0,0 +1,48 @@
+#include "ContainerFS.hpp"
+
+#include <Mocker/Core/Syscall.hpp>
+
+ContainerFS::ContainerFS(const std::string &basePath) : basePath(basePath) {}
+
+std::string
+ContainerFS::GetPath() const
+{
+   return basePath + "/" + subPath;
+}
+
+Result<bool>
+ContainerFS::Init() const
+{
+   Result<int> res { false };
+
+   res = Syscall::MKDIR(basePath, 0755)
+             .WithErrorHandler([this](Ref<Error> error) {
+                if (error->Last().GetCode() == ErrorCode::MKDIR_EXISTED)
+                {
+                   error->Clear();
+                   return;
+                }
+
+                error->Push({ ErrorCode::MKDIR_FAILED,
+                              "Failed to create base directory for mocker " +
+                                  basePath });
+             });
+   if (!res) return { false, res.error };
+
+   res = Syscall::MKDIR(GetPath(), 0755)
+             .WithErrorHandler([this](Ref<Error> error) {
+                if (error->Last().GetCode() == ErrorCode::MKDIR_EXISTED)
+                {
+                   error->Clear();
+                   return;
+                }
+
+                error->Push(
+                    { ErrorCode::MKDIR_FAILED,
+                      "Failed to create directory for mocker containers: " +
+                          GetPath() });
+             });
+   if (!res) return { false, res.error };
+
+   return { true };
+}
---
+++ Source/Mocker/FS/ContainerFS.hpp
@@ -0,0 +1,20 @@
+#pragma once
+
+#include <string>
+
+#include <Mocker/Core/Result.tcc>
+
+class ContainerFS
+{
+   std::string       basePath;
+   const std::string subPath = "containers";
+
+public:
+   ContainerFS(const std::string &basePath);
+
+   std::string
+   GetPath() const;
+
+   Result<bool>
+   Init() const;
+};
